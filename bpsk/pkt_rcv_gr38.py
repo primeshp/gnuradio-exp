5aa5748a97c756ae6f10ae0ea7b825f609509003
Primesh <primeshp@gmail.com>
2023-04-14 14:12:57 -0400

    Added functional BPSK

--- bpsk/pkt_rcv_gr38.py
+++ bpsk/pkt_rcv_gr38.py
@@ -138,46 +138,6 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            firdes.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            1
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(True)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
             1024, #size
             "", #name
@@ -219,7 +179,7 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win)
         self.osmosdr_source_0 = osmosdr.source(
-            args="numchan=" + str(1) + " " + '"rtl=0'
+            args="numchan=" + str(1) + " " + "rtl_tcp=192.168.21.49:1234"
         )
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(usrp_rate)
@@ -230,15 +190,7 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_bb_gain(20, 0)
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(
-            int((usrp_rate/samp_rate)*rs_ratio),
-            firdes.low_pass(
-                1,
-                usrp_rate,
-                65000,
-                5000,
-                firdes.WIN_HAMMING,
-                6.76))
+        self.mmse_resampler_xx_0 = filter.mmse_resampler_cc(0, ((usrp_rate/samp_rate)*rs_ratio))
         self.digital_symbol_sync_xx_0 = digital.symbol_sync_cc(
             digital.TED_GARDNER,
             sps,
@@ -254,8 +206,9 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
         self.digital_map_bb_0 = digital.map_bb([0,1])
         self.digital_lms_dd_equalizer_cc_0 = digital.lms_dd_equalizer_cc(15, 0.000001, 1, bpsk)
         self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(2)
-        self.digital_costas_loop_cc_0 = digital.costas_loop_cc(phase_bw, order, True)
-        self.digital_correlate_access_code_tag_xx_0 = digital.correlate_access_code_tag_bb("11100001010110101110100010010011", 1, 'start')
+        self.digital_costas_loop_cc_0 = digital.costas_loop_cc(phase_bw, order, False)
+        self.digital_correlate_access_code_xx_ts_0 = digital.correlate_access_code_bb_ts("11100001010110101110100010010011",
+          thresh, 'packet_len')
         self.digital_constellation_decoder_cb_0 = digital.constellation_decoder_cb(bpsk)
         self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, 'packet_len')
         self.blocks_repack_bits_bb_1 = blocks.repack_bits_bb(1, 8, "packet_len", False, gr.GR_MSB_FIRST)
@@ -266,20 +219,19 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_1, 'print'))
+        self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_1, 'print_pdu'))
         self.connect((self.blocks_repack_bits_bb_1, 0), (self.blocks_tagged_stream_to_pdu_0, 0))
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.digital_diff_decoder_bb_0, 0))
-        self.connect((self.digital_correlate_access_code_tag_xx_0, 0), (self.blocks_repack_bits_bb_1, 0))
+        self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.blocks_repack_bits_bb_1, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_constellation_decoder_cb_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.digital_diff_decoder_bb_0, 0), (self.digital_map_bb_0, 0))
         self.connect((self.digital_lms_dd_equalizer_cc_0, 0), (self.digital_costas_loop_cc_0, 0))
         self.connect((self.digital_lms_dd_equalizer_cc_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.digital_map_bb_0, 0), (self.digital_correlate_access_code_tag_xx_0, 0))
+        self.connect((self.digital_map_bb_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
         self.connect((self.digital_symbol_sync_xx_0, 0), (self.digital_lms_dd_equalizer_cc_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.digital_symbol_sync_xx_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.mmse_resampler_xx_0, 0), (self.digital_symbol_sync_xx_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.mmse_resampler_xx_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "pkt_rcv_gr38")
@@ -291,7 +243,7 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
 
     def set_usrp_rate(self, usrp_rate):
         self.usrp_rate = usrp_rate
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.usrp_rate, 65000, 5000, firdes.WIN_HAMMING, 6.76))
+        self.mmse_resampler_xx_0.set_resamp_ratio(((self.usrp_rate/self.samp_rate)*self.rs_ratio))
         self.osmosdr_source_0.set_sample_rate(self.usrp_rate)
 
     def get_thresh(self):
@@ -311,7 +263,7 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.mmse_resampler_xx_0.set_resamp_ratio(((self.usrp_rate/self.samp_rate)*self.rs_ratio))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_rs_ratio(self):
@@ -319,6 +271,7 @@ class pkt_rcv_gr38(gr.top_block, Qt.QWidget):
 
     def set_rs_ratio(self, rs_ratio):
         self.rs_ratio = rs_ratio
+        self.mmse_resampler_xx_0.set_resamp_ratio(((self.usrp_rate/self.samp_rate)*self.rs_ratio))
 
     def get_phase_bw(self):
         return self.phase_bw
